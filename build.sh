97f158cc35aeb72ed9ae0308be6bea2487949136
anyone <anyone@renwu.cool>
2020-05-11 05:19:28 +0800

    master

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")
 
 # 要想使用 buildx，首先要确保 Docker 版本不低于 19.03，同时还要通过设置环境变量 DOCKER_CLI_EXPERIMENTAL 来启用。
-#export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # docker buildx create \
 #   --use \
@@ -13,6 +13,6 @@ cd $(dirname "$0")
 # # #  --driver-opt env.ENV_NAME=ENV_VALUE
 # docker buildx use insecure-builder
 # docker buildx build --allow security.insecure -t renwucool/dev .
-docker build -t renwucool/dev .
+docker build --squash -t renwucool/dev .
 # docker buildx build -t renwu/dev .
 # docker buildx create --use --name insecure-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure'
