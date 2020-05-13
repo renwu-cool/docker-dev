2723ca80d05117e4e5304d859c3f167f44802bf7
anyone <anyone@renwu.cool>
2020-05-13 10:32:23 +0800

    master

--- os/root/.zplugin.zsh
+++ os/root/.zplugin.zsh
@@ -1,29 +1,29 @@
 source ~/.zplugin/bin/zplugin.zsh
 
-zplugin ice wait lucid
-zplugin light skywind3000/z.lua
+zinit ice wait lucid
+zinit light skywind3000/z.lua
 
 export FZ_HISTORY_CD_CMD=_zlua
-zplugin ice wait lucid
-zplugin light changyuheng/fz
+zinit ice wait lucid
+zinit light changyuheng/fz
 
-zplugin ice wait lucid
-zplugin light changyuheng/zsh-interactive-cd
+zinit ice wait lucid
+zinit light changyuheng/zsh-interactive-cd
 
-zplugin ice wait lucid
-zplugin light zdharma/fast-syntax-highlighting
+zinit ice wait lucid
+zinit light zdharma/fast-syntax-highlighting
 
-zplugin ice wait lucid atload'_zsh_autosuggest_start'
-zplugin light zsh-users/zsh-autosuggestions
+zinit ice wait lucid atload'_zsh_autosuggest_start'
+zinit light zsh-users/zsh-autosuggestions
 
-zplugin ice wait lucid
-zplugin light zdharma/history-search-multi-word
+zinit ice wait lucid
+zinit light zdharma/history-search-multi-word
 
 PURE_GIT_DOWN_ARROW='↓'
 PURE_GIT_UP_ARROW='↑'
 
-zplugin ice pick"async.zsh" src"pure.zsh"
-zplugin light sindresorhus/pure
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
 
 #typeset -A pure_halloween_scheme=(
 #	color1 "#888" # English Vermillion
@@ -52,12 +52,12 @@ zstyle :prompt:pure:host color 237
 zstyle :prompt:pure:user:root  color 237
 zstyle :prompt:pure:git:branch color 237
 
-zplugin ice wait lucid as"program" pick"$ZPFX/bin/git-*" make"PREFIX=$ZPFX"
-zplugin light tj/git-extras
+zinit ice wait lucid as"program" pick"$ZPFX/bin/git-*" make"PREFIX=$ZPFX"
+zinit light tj/git-extras
 
 bindkey "^A" vi-beginning-of-line
 bindkey "^E" vi-end-of-line
 
-zplugin ice atload'!_zsh_git_prompt_precmd_hook' lucid
-zplugin light woefe/git-prompt.zsh
+zinit ice atload'!_zsh_git_prompt_precmd_hook' lucid
+zinit light woefe/git-prompt.zsh
 
